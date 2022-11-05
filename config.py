e3d8449c244a903a2c2854e3736e2b41a6e2fe7c
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2022-11-05 10:32:35 -0400

    ADD: Connection to the local database

---
+++ config.py
@@ -0,0 +1,16 @@
+#Load config from environment variables
+from os import environ, path
+from dotenv import load_dotenv
+
+
+# Load variables from .env
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+# Database config
+DATABASE_HOST = environ.get('DATABASE_HOST')
+DATABASE_USERNAME = environ.get('DATABASE_USERNAME')
+DATABASE_PASSWORD = environ.get('DATABASE_PASSWORD')
+DATABASE_PORT = environ.get('DATABASE_PORT')
+DATABASE_NAME = environ.get('DATABASE_NAME')
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hola mundo")
+from config import (
+    DATABASE_HOST,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    DATABASE_PORT,
+    DATABASE_NAME,
+)
+import psycopg2
+conn = None
+
+# DATABASE CONNECTION
+try:
+    conn = psycopg2.connect(
+        host=DATABASE_HOST,
+        user=DATABASE_USERNAME,
+        password=DATABASE_PASSWORD,
+        port=DATABASE_PORT,
+        dbname=DATABASE_NAME
+    )
+except psycopg2.DatabaseError as e:
+    print(e)
+
+# TEST CONNECTION 
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM stock")
+data = cursor.fetchall()
+
+print(data)
+
